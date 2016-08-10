5a17d2c728a97ac2479cdae2252598ff741e09b6
Aaron Russo <arusso@berkeley.edu>
2016-08-10 15:57:32 -0700

    Add support for setting lita.robot.admins via LITA_ADMINS envvar

--- data/lita_config.rb
+++ data/lita_config.rb
@@ -6,6 +6,7 @@ Lita.configure do |config|
   config.robot.alias = "."
   config.robot.log_level = :info
   config.robot.adapter = :slack
+  config.robot.admins = ENV['LITA_ADMINS'].split(',')
   config.adapters.slack.token = "#{ENV['LITA_SLACK_TOKEN']}"
   config.redis[:host] = "#{ENV['LITA_REDIS_HOST']}"
   config.handlers.dig.default_resolver = '127.0.0.1'
