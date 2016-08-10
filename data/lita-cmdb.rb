a666084f75548ae377361a620e94971f5946afbd
Aaron Russo <arusso@berkeley.edu>
2016-08-10 15:58:23 -0700

    Rewrite contact and sla retrieval routes

--- data/lita-cmdb.rb
+++ data/lita-cmdb.rb
@@ -5,28 +5,30 @@ module Lita
       config :itop_user
       config :itop_pass
 
-      # respond to contact queries
       route(
-        /^contacts\ get\ (?<service_id>[a-zA-Z0-9\-]+)(\s+(?<contact_type>[a-z]+))?$/,
-        :get_contacts,
-        command: true,
-        help: {
-          'contacts get <service id> <contact type>' => 'Returns contact information for service id'
+        /^(?:get\ )?contacts\ (?:for\ )?(?:host\ )?(?<service_id>[a-zA-Z0-9\-\_]+)(\ of)?(\ type)?(?:\ (?<contact_type>technical|owner|billing|authorized|all))?$/,
+	:get_contacts,
+	command: true,
+	help: {
+          '[get] contacts [for host] <service_id> [[of type ] <contact_type>]' => 'Lookup contacts for the given service id. You can optionally specify a contact type. Types are: all, authorized (owner + technical, default), billing, owner, technical',
+          'contacts <service_id> [<contact_type>]' => 'Shorthand notation for contact lookup',
         },
       )
 
       # respond to sla queries
       route(
-        /^sla\ (?<service_id>[a-zA-Z0-9\-]+)$/,
+        /^(?:get\ )?sla\ (?:(?:for|of)\ )?(?:(?:host|service\ id)\ )?(?<service_id>[a-zA-Z0-9\-]+)$/,
         :get_sla,
         command: true,
         help: {
-          'sla <service id>' => "Returns SLA for service id"
+          '[get] sla [for host] <service_id>' => "Returns SLA for service id",
+          'sla <service_id>' => 'Shorthand notation for SLA lookup',
         },
       )
      
       def get_sla(response)
         service_id = response.match_data['service_id']
+        puts "#{response.user.mention_name} << get_sla(service_id: #{service_id})"
         query = [ 'SELECT SLA AS s',
               'JOIN lnkCustomerContractToService AS l1',
               'ON l1.sla_id=s.id',
@@ -55,75 +57,95 @@ module Lita
           end 
         end
 
+        log_msg="#{response.user.mention_name} >> sla is "
         if sla == ''
+          log_msg+= "unknown"
           response.reply("unable to determine sla for #{service_id}")
         else
+          log_msg+= "#{sla}"
           response.reply("#{service_id} has #{sla} support")
         end
+        puts log_msg
       end
 
       def get_contacts(response)
-        response.reply("Contact listing is unavailable at this time... for realz.")
-        return
-
         sid = response.match_data['service_id']
-        type = case response.match_data['contact_type']
+        req_type = response.match_data['contact_type']
+        puts "#{response.user.mention_name} << get_contacts(service_id: #{sid}, contact_type: #{req_type})"
+        
+        case req_type
         when 'billing'
-          "Billing Contact"
+          contact_types = [ req_type ]
         when 'technical'
-          "Technical Contact"
+          contact_types = [ req_type ]
         when 'owner'
-          "Functional Owner"
+          contact_types = [ req_type ]
+        when 'authorized'
+          contact_types = [ 'owner', 'technical' ]  
+        when 'all'
+          contact_types = [ 'owner', 'technical', 'billing' ]
         when nil
-          "Technical Contact"
+          contact_types = [ 'owner', 'technical' ]
         else
-          "UNKNOWN"
-        end
-
-        if type == "UNKNOWN"
-          response.reply("Unknown contact type #{response.match_data['contact_type']}")
+          response.reply("Sorry, I don't know how to process contact type: #{req_type}")
           return
-        else
-          response.reply("Looking up contacts for #{sid}/#{type}")
         end
 
+        contacts = {}
+        contact_types.each do |type|
+          contacts_of_type = get_contacts_for_service_id_of_type(sid,type)
+          contacts_of_type.each do |id,info|
+            if contacts[id].nil?
+              contacts[id] = info
+              contacts[id]['type'] = type
+            else
+              contacts[id]['type'] += ",#{type}"
+            end
+          end
+        end
+
+        if contacts.size > 0
+          reply = "Contacts for service id *#{sid}*:\n"
+          contacts.each { |id,info| reply += " • #{info['email']} (#{info['type']})\n" }
+        else
+          reply = "No #{req_type} contacts found for #{sid}" 
+        end
+        puts "#{response.user.mention_name} >> found #{contacts.size} contacts"
+        response.reply(reply)
+      end
+
+      private
+      # return a hash containing
+      def get_contacts_for_service_id_of_type(service_id, type)
         query = [
           "SELECT Person AS p",
           "JOIN lnkPersonToTeam AS l1 ON l1.person_id=p.id",
           "JOIN Team AS t ON l1.team_id=t.id",
           "JOIN lnkContactToFunctionalCI AS l2 ON l2.contact_id=t.id",
           "JOIN VirtualMachine AS v ON l2.functionalci_id=v.id",
-          "WHERE v.name REGEXP '^#{sid}$'",
-          "AND t.name LIKE '#{type}'"
+          "WHERE v.name REGEXP '^#{service_id}$'",
+          "AND l1.role_name LIKE '%#{type}%'"
         ].join(' ')
-
         data = {
           'operation' => 'core/get',
           'class' => 'Person',
           'key' => query,
-          'output_fields' => 'email',
+          'output_fields' => 'email,employee_number,friendlyname',
         }
-
         http_res = query_itop(data)
-
         res = MultiJson.load(http_res.body)
-        people = []
+        people = {}
         
-        response.reply("Got: #{http_res.body}")
-        res['objects'].each { |person,data| people << data['fields']['email'] }
-	people.uniq!
- 
-        reply = ""
-        if people.length > 0
-          reply = "Contacts for service id #{service_id} are #{people.join(', ')}"
-        else
-          reply = "No contacts found for #{service_id}"
-        end
-
-        response.reply(reply)
+        res['objects'].each do |person,data|
+          continue if data['fields']['employee_number'].nil?
+          people[data['fields']['employee_number']]= {
+            'email' => data['fields']['email'],
+            'full_name' => data['fields']['friendlyname'],
+          }
+        end unless res['objects'].nil?
+        people
       end
 
-      private
       def query_itop(data)
         json_data = MultiJson.dump(data)
         conn = Faraday::Connection.new
